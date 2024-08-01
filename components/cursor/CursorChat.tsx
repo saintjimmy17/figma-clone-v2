c24b5ea05fc180ae35164d83d264ac0cd9ec4b9c
Marian <italianomariano198@gmail.com>
2024-08-01 16:07:25 -0300

    active users and sidebard added

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -31,7 +31,9 @@ const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence}: Cu
         <>
           <CursorSVG color='#000' />
 
-          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'>
+          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'
+            onKeyUp={(e) => e.stopPropagation()}
+          >
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
             )}
