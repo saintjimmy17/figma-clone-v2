95bcd4e5c5c288f327971753d6a0d58ae62d1865
Marian <italianomariano198@gmail.com>
2024-08-02 12:59:39 -0300

    canvas and toolbar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'liveblocks.io',
-                port: ''
-            }
-        ]
-    }  
-};
-
-export default nextConfig;
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'liveblocks.io',
+          port: '',
+        },
+      ],
+    },
+    webpack: (config, { isServer }) => {
+      // Add custom rule for .node files
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+  
+      // Add this to ensure it works for both server and client
+      if (isServer) {
+        config.externals = config.externals || [];
+        config.externals.push(({ context, request }, callback) => {
+          if (/\.node$/.test(request)) {
+            callback(null, 'commonjs ' + request);
+          } else {
+            callback();
+          }
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
